059f48a1b19c15bf06f50f4ffddcb4673dc155bd
Nao Komura <nao.komura@sixaxd.com>
2019-05-06 04:39:15 +0900

    練習終わり

--- web-api-test/app.js
+++ web-api-test/app.js
@@ -24,23 +24,16 @@ async function requestApi() {
 
 async function exportDom() {
   const reqObj = await requestApi()
-  test.textContent = reqObj[0].id
+
+  for (const item of reqObj) {
+    const h1 = document.createElement('h1')
+    const p = document.createElement('p')
+    h1.appendChild(document.createTextNode(item.title))
+    p.appendChild(document.createTextNode(item.body))
+    test.appendChild(h1)
+    test.appendChild(p)
+  }
+  console.log(reqObj)
 }
 
 exportDom()
-
-// function getIssuesRecursively(res) {
-//   issues.push(...res.data)
-//   if (!res.headers.link) return issues
-//   const next = parseLinkHeader(res.headers.link).next
-//   if (!next) return issues
-//   return request.get(next).then(res => getIssuesRecursively(res))
-// }
-
-// function parseLinkHeader(linkHeader) {
-//   return linkHeader.split(',').reduce((ret, linkStr) => {
-//     const [_, url, __, rel] = linkStr.match(/<(.*?)>;\s?(rel="(.*?)")?/)
-//     if (rel) ret[rel] = url
-//     return ret
-//   }, {})
-// }
